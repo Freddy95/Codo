75dc075655da91df084d8528b6b8052cd3674a6b
Matt Del Signore <mattdelsig@me.com>
2017-03-14 18:37:38 -0400

    added LoginController
    :

---
+++ src/main/com/dolphinblue/codo/controller/LoginController.java
@@ -0,0 +1,6 @@
+package com.dolphinblue.codo.controller;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import java.io.IOException;
