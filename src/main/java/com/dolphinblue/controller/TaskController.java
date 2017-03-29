b37dce3b4206523e95d8fc741fbd300fd6640c58
Festevez <f.estevez95@gmail.com>
2017-03-29 11:03:03 -0400

    Added UserController and TaskController

---
+++ src/main/java/com/dolphinblue/controller/TaskController.java
@@ -0,0 +1,20 @@
+package com.dolphinblue.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by FreddyEstevez on 3/29/17.
+ * This controller should handle all requests for the task page.
+ *
+ * Request for Task
+ * Request for Editor Blocks
+ * Request for Toolbox Blocks
+ * Update Block Order
+ * Update Block Location for if it moves from Toolbox to Editor and the other way around
+ * Restart Task
+ * Restart Lesson
+ */
+@Controller
+public class TaskController {
+
+}
---
+++ src/main/java/com/dolphinblue/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.dolphinblue.controller;
+import com.dolphinblue.models.User;
+import com.dolphinblue.service.OfyService;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Objectify;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by FreddyEstevez on 3/29/17.
+ * This controller should handle all requests for the user page.
+ *
+ * Main Lessons Request
+ * Name Request
+ * Percent Complete (calculated from tasks completed)
+ * Current Lesson Request
+ */
+@Controller
+public class UserController {
+
+    /**
+     * gets user and adds him to @param model
+     * @return
+     */
+    @RequestMapping(value = "/userpage", method = RequestMethod.GET)
+    public String get_user_page(@RequestParam(value = "id") Long id, Model model){
+        Objectify ofy = OfyService.ofy();
+
+        User user = ofy.load().type(User.class).id(id).now();
+        model.addAttribute("user", user);
+        return "UserPage";
+    }
+
+}
