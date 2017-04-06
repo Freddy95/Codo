83381c1f2839d7170082c28d5bf249663e7ad9cd
Henry <henry.chin@stonybrook.edu>
2017-04-05 20:49:20 -0400

    Added block-ids

--- src/main/webapp/public/js/draggable.js
+++ src/main/webapp/public/js/draggable.js
@@ -7,49 +7,6 @@ var next_task = "";
 var task_id = "";
 var lesson_id = "";
 
-function run() {
-  // Empty the output when running.
-  $('#output').empty();
-
-  // Redirect console.log and window.one-error to output.
-  window.console.log = function(msg) {
-    $('#output').append(document.createTextNode(msg)).append($('<br />'));
-  }
-
-  window.onerror = function(messageOrEvent, source, lineno, colno, error) {
-    $('#output').text(messageOrEvent);
-  }
-
-  // Evaluating code.
-  $.each($('#editor').children(), function(index, value) {
-    try {
-      eval($(value).text());
-    }
-    // Flash on a block that errors.
-    catch(e) {
-      $(value).addClass('flash');
-      setTimeout( function(){
-        $(value).removeClass('flash');
-      }, 1000);
-      throw(e);
-    }
-  });
-
-  /* Adds a next arrow if it doesn't exist already and if the solution is correct.
-   */
-  if ($('#output-div>.card-title-block').children().length === 1 &&
-      $('#output').html() === expected_output) {
-    // Adding next arrow to next task.
-    if (next_task > 0) {
-      $('#output-div>.card-title-block').append($('<a id="next-arrow" class="fa fa-lg fa-vc fa-arrow-right pull-right" href="/lesson/' + lesson_id + '/task/' + next_task + '"></a>'));
-    }
-    // If last lesson, just redirect to user page.
-    else {
-      $('#output-div>.card-title-block').append($('<a id="next-arrow" class="fa fa-lg fa-vc fa-arrow-right pull-right" href="/user"></a>'));
-    }
-  }
-}
- 
 function init() {
   // Get the div for the page, we'll use it to get the data attribute
   // Use the dollar sign to signify that page is a jquery dom element
@@ -80,4 +37,50 @@ function init() {
       }
     }
   }).disableSelection();
+
+  function run() {
+    // Empty the output when running.
+    $('#output').empty();
+
+    // Redirect console.log and window.one-error to output.
+    window.console.log = function(msg) {
+      $('#output').append(document.createTextNode(msg)).append($('<br />'));
+    }
+
+    window.onerror = function(messageOrEvent, source, lineno, colno, error) {
+      $('#output').text(messageOrEvent);
+    }
+
+    // Evaluating code.
+    $.each($('#editor').children(), function(index, value) {
+      try {
+        eval($(value).text());
+      }
+      // Flash on a block that errors.
+      catch(e) {
+        $(value).addClass('flash');
+        setTimeout( function(){
+          $(value).removeClass('flash');
+        }, 1000);
+        throw(e);
+      }
+    });
+
+    /* Adds a next arrow if it doesn't exist already and if the solution is correct.
+     */
+    if ($('#output-div>.card-title-block').children().length === 1 &&
+        $('#output').html() === expected_output) {
+      // Adding next arrow to next task.
+      if (next_task > 0) {
+        $('#output-div>.card-title-block').append($('<a id="next-arrow" class="fa fa-lg fa-vc fa-arrow-right pull-right" href="/lesson/' + lesson_id + '/task/' + next_task + '"></a>'));
+      }
+      // If last lesson, just redirect to user page.
+      else {
+        $('#output-div>.card-title-block').append($('<a id="next-arrow" class="fa fa-lg fa-vc fa-arrow-right pull-right" href="/user"></a>'));
+      }
+    }
+  }
+
+  function save() {
+  }
 }
