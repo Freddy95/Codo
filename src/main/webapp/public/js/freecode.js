a07122295fbec31e7b291643ec2b50e63e8bd054
Matt Del Signore <mattdelsig@me.com>
2017-04-13 17:43:39 -0400

    Added code editor, as well as methods to run the code and clean the code on the frontend.

--- src/main/webapp/public/js/freecode.js
+++ src/main/webapp/public/js/freecode.js
@@ -4,9 +4,42 @@
  */
 
 //shorthand for document.ready
-$(function(){
+$(function () {
+    //set up the ace editor
     var editor = ace.edit("freecodeeditor");
-    editor.setTheme("ace/theme/monokai");
+    editor.setTheme("ace/theme/chrome");
     editor.getSession().setMode("ace/mode/javascript");
 
+
+    //attach event handlers to some of the buttons
+    var $runbtn = $("#runbutton");
+    $runbtn.click( function () {
+
+    // Empty the output when running.
+    $('#output').empty();
+
+    // Redirect console.log and window.one-error to output.
+    // var former = window.console.log;
+    window.console.log = function (msg) {
+        $('#output').append(document.createTextNode(msg)).append($('<br />'));
+    };
+
+    window.onerror = function (messageOrEvent, source, lineno, colno, error) {
+        $('#output').text(messageOrEvent);
+    };
+        //pull out any xhr requests
+        var code = editor.getValue();
+        code = clean(code);
+        // run the cleaned code
+        eval(code);
+    });
 });
+
+//cleans up the the code using regex
+function clean(code) {
+    //code : the string of js code we need to clean up and ensure is not malicious
+    var regex = /(XMLHttpRequest)|(xhr\.\w*)|(\$\.\w*)|(document\.getElementBy\w*\(\w*\))|(document\.getElementsBy\w*\(\w*\))/;
+    //replace the offending functions and return the sanitized code
+    return code.replace(regex, " ");
+
+}
