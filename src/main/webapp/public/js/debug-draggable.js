06ff1194f26b0af14e206dfa89aacc0dd27f1853
Henry <henry.chin@stonybrook.edu>
2017-04-13 18:04:50 -0400

    Adding files i forgot to add

---
+++ src/main/webapp/public/js/debug-draggable.js
@@ -0,0 +1,155 @@
+$(document).ready(function() {
+  init();
+});
+
+// Strings for grabbing values from Thymeleaf Template.
+var test_case = "";
+var expected_output = "";
+var next_task = "";
+var task_id = "";
+var lesson_id = "";
+var completed = "";
+
+function resize_content() {
+  console.log('Resizing');
+  padding = 10;
+  border = 1;
+
+  // Calculate the height to resize the row to.
+  content_height = 0;
+
+  // Calculate the size of the editor.
+  $('#editor-div').children().each(function() {
+    content_height += $(this).outerHeight();
+  })
+  content_height += $('#editor-div').outerHeight(true) - $('#editor-div').height();
+
+  // Calculate the size of the toolbox.
+  $('#toolbox-div').children().each(function() {
+    content_height += $(this).outerHeight();
+  })
+  content_height += $('#toolbox-div').outerHeight(true) - $('#toolbox-div').height();
+
+  // Get the padding for the row.
+  block_outer = $('#block-content').parent().outerHeight(true) - $('#block-content').parent().height();
+  console.log(block_outer);
+  content_height += block_outer;
+
+  // Calculate the height left-over in the window.
+  window_height = $(window).height();
+
+  // Subtract the height of the navbars.
+  $('#lesson-content').siblings().each(function() {
+    window_height -= $(this).outerHeight(true);
+  });
+
+  // Take the maximum of window and content height.
+  $('#lesson-content').css("height", Math.max(window_height, content_height) + "px");
+
+  // Resize block content.
+  console.log($('#block-content').height());
+  $('#block-content').css("height", (Math.max(window_height, content_height) - block_outer) + "px");
+  console.log($('#block-content').height());
+}
+
+function init() {
+  resize_content();
+
+  // Get the div for the page, we'll use it to get the data attribute
+  // Use the dollar sign to signify that page is a jquery dom element
+  var $page_info = $("#page-info");
+
+  // Get the data attributes for the data needed in this javascript file.
+  test_case = $page_info.data("test-case");
+  expected_output = $page_info.data("ex-output");
+  next_task = $page_info.data('next-task');
+  task_id = $page_info.data('task-id');
+  lesson_id = $page_info.data('lesson-id');
+  completed = $page_info.data('completed');
+
+  // Remove the element once done loading from the page.
+  $page_info.remove();
+
+  /* Expected output has newlines, we'll turn them in to <br> so it works
+   * with html
+   */
+  expected_output = String(expected_output).replace("\n","<br>") + "<br>";
+
+  /* Makes child elements of editor, toolbox, and holds-one draggable
+   * between all elements of those types.
+   */  
+  $('#editor, #toolbox, .holds-one').sortable({
+    connectWith: ".code-placement",
+    over: function(e, ui){ 
+        resize_content();
+    },
+    receive: function(event, ui) {
+      // Restrict .holds-one to hold one element at a time.
+      if ($(this).hasClass('holds-one')) {
+        if ($(this).children().length > 1) {
+          $(ui.sender).sortable('cancel');
+        }
+      }
+    }
+  }).disableSelection();
+}
+
+function getCodeBlock(node) {
+  return $(node).children().children().children();
+}
+
+function getCodeBlockAttr(value) {
+  block = {};
+  block.block_id = parseInt($(value).attr('id'));
+  if (!($(value).attr('data-children'))) {
+    block.value = $(value).text();
+  }
+  else {
+    children = [];
+    $.each(getCodeBlock($(value)), function(index, v) {
+      child_block = getCodeBlockAttr(v);
+      children.push(child_block);
+    });
+    block.children = children;
+  }
+  return block;
+}
+
+function save() {
+  var data = {};
+
+  var editor = [];
+  var toolbox = [];
+
+  $.each(getCodeBlock($('#editor')), function(index, value) {
+    var block = getCodeBlockAttr(value);
+    editor.push(block);
+  });
+
+  $.each(getCodeBlock($('#toolbox')), function(index, value) {
+    var block = getCodeBlockAttr(value);
+    toolbox.push(block);
+  });
+
+  // data.task_id = task_id;
+  data.editor = editor;
+  data.toolbox = toolbox;
+  data.completed = completed;
+
+  console.log(data);
+
+  $.ajax({
+    headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json' 
+    },
+    'type': 'POST',
+    'url': 'savecreatedlesson/1/task/1',
+    'data': JSON.stringify(data),
+    'dataType': 'json'
+  }).done(function() {
+    return true;
+  }).fail(function() {
+    return false;
+  });
+}
