17da3fedeb159a0f9874d80e27012e63075a25c4
Devon Maguire <maguiredm@gmail.com>
2017-04-17 21:29:22 -0400

    Added the tutorial to the first task page. It goes through all of the buttons and areas that the user can interact with (though the dialogue may need some work). The only thing left is to add in a boolean to check if the tutorial should be played. I commented out the starting point for both tutorials so nethier will play. Uncomment these lines if you would like to play the tutorial: FILE user.js LINES 94-96, FILE draggable.js LINE 321.

--- src/main/webapp/public/js/draggable.js
+++ src/main/webapp/public/js/draggable.js
@@ -125,6 +125,8 @@ function init() {
       }
     }
   }).disableSelection();
+
+    startTutorial();
 }
 
 function run() {
@@ -223,4 +225,98 @@ function save() {
   }).fail(function() {
     return false;
   });
+}
+
+function startTutorial() {
+    // Setup the tutorial intro for the user page
+    var tutorial = introJs();
+    tutorial.setOptions({
+        steps: [
+            {
+                // Focus: instructions
+                element: '.step-1',
+                intro: 'These are the instructions for completing the task you are on. Follow these instructions closely to move on in the lesson.',
+                position: 'right'
+            },
+            {
+                // Focus: editor
+                element: '.step-2',
+                intro: 'This is the Editor where you will build your program using code blocks',
+                position: 'left'
+            },
+            {
+                // Focus: toolbox
+                element: '.step-3',
+                intro: 'By dragging the blocks from this Toolbox into the Editor you can make programs.',
+                position: 'top'
+            },
+            {
+                // Focus: run button
+                element: '.step-4',
+                intro: 'To test your program to see if you are ready to move on to the next task, click this button and it will run the code you built with blocks in the Editor.',
+                position: 'right'
+            },
+            {
+                // Focus: output
+                element: '.step-5',
+                intro: 'This box will display the output given by running your program. This output needs to be the correct output as specified in the instructions in order to move on to the next task.',
+                position: 'left'
+            },
+            {
+                // Focus: hint button
+                element: '.step-6',
+                intro: 'If you are having trouble getting the right output for your program, click this button to get a hint.',
+                position: 'right'
+            },
+            {
+                // Focus: save task
+                element: '.step-7',
+                intro: 'You can click this button at any time to save the progress you\'ve made.',
+                position: 'left'
+            },
+            //{
+                // Focus: previous task
+                //element: '.step-8',
+                //intro: 'To navigate back to the previous task in this lesson click this button.',
+                //position: 'bottom'
+            //},
+            {
+                // Focus: undo changes
+                element: '.step-9',
+                intro: 'Pressing this button will undo all of the changes made since your last save.',
+                position: 'bottom'
+            },
+            {
+                // Focus: restart task
+                element: '.step-10',
+                intro: 'To restart the task you are on from the beginning click this button.',
+                position: 'bottom'
+            },
+            {
+                // Focus: restart lesson
+                element: '.step-11',
+                intro: 'To restart all of the tasks in the lesson click this button.',
+                position: 'bottom'
+            },
+            {
+                // Focus: task navigation
+                element: '.step-12',
+                intro: 'Once you have completed a task you can navigate to it directly at anytime using this task navigation bar. Hover over the numbers in the bar to get the title of that particular task.',
+                position: 'bottom'
+            },
+            {
+                // Focus: home button
+                element: '.step-13',
+                intro: 'To go back to the home page where you were before, click this button. You\'re now ready to complete your first task!',
+                position: 'bottom'
+            }
+        ]
+    });
+
+    // Setup the page transition for the tutorial to navigate home?
+    //tutorial.setOption('doneLabel', 'Start Lesson').start().oncomplete(function() {
+    //    window.location.href = "/lesson/" + lesson_id;
+    //});
+
+    //tutorial.start();
 }
