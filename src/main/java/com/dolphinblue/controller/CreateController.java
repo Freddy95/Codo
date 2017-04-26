a94f6606abbd8bc369e7181a9073704b0837b865
Festevez <f.estevez95@gmail.com>
2017-04-26 10:35:50 -0400

    created delete task route, needs to be tested. Added methods in TaskService.java to delete a task from a lesson, and get which task should be now the current task to be edited when a task is deleted.

--- src/main/java/com/dolphinblue/controller/CreateController.java
+++ src/main/java/com/dolphinblue/controller/CreateController.java
@@ -3,10 +3,10 @@ package com.dolphinblue.controller;
 import com.dolphinblue.models.Lesson;
 import com.dolphinblue.models.SaveTaskModel;
 import com.dolphinblue.models.Task;
-import com.dolphinblue.service.AuthenticationService;
-import com.dolphinblue.service.CodoUserService;
-import com.dolphinblue.service.LessonService;
-import com.dolphinblue.service.OfyService;
+import com.dolphinblue.service.*;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.googlecode.objectify.Key;
 import com.googlecode.objectify.Objectify;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,9 +27,24 @@ public class CreateController {
     AuthenticationService authenticationService;
     @Autowired
     CodoUserService userService;
+    @Autowired
+    TaskService taskService;
 
+    /**
+     * Saves the current block - task in the created lesson
+     * @param token -- user access token
+     * @param id -- lesson id
+     * @param taskId -- id of task to be saved
+     * @param blocks -- list of blocks to be saved
+     * @return
+     */
     @RequestMapping(value = "/savecreatedlesson/{lessonId}/task/{taskId}", method = RequestMethod.POST)
-    public @ResponseBody SaveTaskModel saveCreatedLesson(@CookieValue("token") String token, @PathVariable(value = "lessonId") long id, @PathVariable(value = "taskId") long taskId, @RequestBody SaveTaskModel blocks){
+    public @ResponseBody String save_created_lesson(@CookieValue("token") String token, @PathVariable(value = "lessonId") long id, @PathVariable(value = "taskId") long taskId, @RequestBody SaveTaskModel blocks){
+        boolean isAuthenticated = authenticationService.isAuthenticated(token,new JacksonFactory(),new NetHttpTransport());
+        if(!isAuthenticated){
+            // If the user isn't properly authenticated send them back to the login page
+            return "redirect:/login";
+        }
         System.out.println("toolbox");
         System.out.println(blocks.getToolbox());
         System.out.println("editor");
@@ -40,10 +55,17 @@ public class CreateController {
         for(int i = 0; i < blocks.getToolbox().getBlocks().size(); i++){
             System.out.println(blocks.getToolbox().getBlocks().get(i));
         }
-        return blocks;
+        return "createtask";
     }
+
+    /**
+     * This route should be called when a user first wants to create a new lesson.
+     * Creates a lesson and task object, saves them in datastore.
+     * @param model -- thymeleaf model
+     * @return -- createtask page.
+     */
     @RequestMapping(value = "/createlesson", method = RequestMethod.GET)
-    public String getCreateLessonPage(Model model){
+    public String get_create_lesson_page(Model model){
         Objectify ofy = OfyService.ofy();
         Lesson l = new Lesson();
         Task t  = new Task();
@@ -54,5 +76,44 @@ public class CreateController {
         model.addAttribute("lesson", l);
         model.addAttribute("task", t);
         return "createtask";
+    }
+
+    /**
+     * Deletes the current task in the lesson user is creating.
+     * @param token -- user access token
+     * @param id -- lesson id
+     * @param taskId -- id of task to delete
+     * @param model -- thymeleaf model
+     * TODO: return create task page?
+     * @return --
+     */
+    @RequestMapping(value = "/savecreatedlesson/{lessonId}/task/{taskId}", method = RequestMethod.DELETE)
+    public String delete_task(@CookieValue("token") String token,  @PathVariable(value = "lessonId") long id,  @PathVariable(value = "taskId") long taskId, Model model){
+
+        boolean isAuthenticated = authenticationService.isAuthenticated(token,new JacksonFactory(),new NetHttpTransport());
+        if(!isAuthenticated){
+            // If the user isn't properly authenticated send them back to the login page
+            return "redirect:/login";
+        }
+
+        Objectify ofy = OfyService.ofy();
+        //get key of task
+        Key task_key = Key.create(Task.class, taskId);
+        //delete task from datastore
+        ofy.delete().type(Task.class).id(taskId).now();
+
+        //Get lesson and delete the task key from the list of tasks.
+        Lesson lesson = ofy.load().type(Lesson.class).id(id).now();
+
+        //delete task and get index of deleted task
+        int index = taskService.delete_task(lesson, task_key);
+
+        //find which task to add to model
+        Task task = taskService.get_edit_task(lesson, index);
+        model.addAttribute("task", task);
+
+        //add lesson to model
+        model.addAttribute("lesson", lesson);
+        return "";
     }
 }
