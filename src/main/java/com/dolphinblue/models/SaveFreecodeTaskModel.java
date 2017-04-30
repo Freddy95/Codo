06dd63002bda1c3522c32e5f2f2f728c1193d5c0
Devon Maguire <maguiredm@gmail.com>
2017-04-30 16:54:11 -0400

    Update the SaveFreeCodeTaskModel so it reflects the same fields as SaveTaskModel except it does not have a toolbox or an editor list of blocks, instead it has a freecode string.

--- src/main/java/com/dolphinblue/models/SaveFreecodeTaskModel.java
+++ src/main/java/com/dolphinblue/models/SaveFreecodeTaskModel.java
@@ -1,12 +1,42 @@
 package com.dolphinblue.models;
 
+import java.util.List;
+
 /**
  * Created by FreddyEstevez on 4/16/17.
  * Used to retrieve freecode task from front end.
  */
 public class SaveFreecodeTaskModel {
-    private String freecode;
+
     private boolean completed;
+    private String freecode;
+    private String hint;
+    private String title;
+    private String instructions;
+    private List<String> test_case;
+    private List<String> expected_output;
+
+    public SaveFreecodeTaskModel() {
+
+    }
+
+    public SaveFreecodeTaskModel(boolean completed, String freecode, String hint, String title, String instructions, List<String> test_case, List<String> expected_output) {
+        this.completed = completed;
+        this.freecode = freecode;
+        this.hint = hint;
+        this.title = title;
+        this.instructions = instructions;
+        this.test_case = test_case;
+        this.expected_output = expected_output;
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 
     public String getFreecode() {
         return freecode;
@@ -16,11 +46,43 @@ public class SaveFreecodeTaskModel {
         this.freecode = freecode;
     }
 
-    public boolean getCompleted() {
-        return completed;
+    public String getHint() {
+        return hint;
     }
 
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public List<String> getTest_case() {
+        return test_case;
+    }
+
+    public void setTest_case(List<String> test_case) {
+        this.test_case = test_case;
+    }
+
+    public List<String> getExpected_output() {
+        return expected_output;
+    }
+
+    public void setExpected_output(List<String> expected_output) {
+        this.expected_output = expected_output;
     }
 }
