dfcb5d9221fd52d39c02b3c051da4b17d74cbb2f
Matt Del Signore <mattdelsig@me.com>
2017-04-30 21:45:05 -0400

    changed the edit lesson stuff to use form groups

--- src/main/webapp/public/js/edit-lesson.js
+++ src/main/webapp/public/js/edit-lesson.js
@@ -3,8 +3,35 @@
  * Javascript for the edit lesson page
  */
 $(function(){
-    $( "#sortable" ).sortable({
+    //get the elements we need to build the session object
+    var $publicRadio = $("#publicradio") ,
+        $descriptionText=$("#description-text"),
+        $submiteDescButton=$("#submit-description-button"),
+        $titleText=$("#title-edit-form"),
+        $titleSubmitButton=$("#title-submit-btn"),
+        $taskList=$("#tasks-list"),
+        $titleEditDone=$("#title-done"),
+        $titleEditButton=$("#title-button"),
+        $showTitleEditFormButton=$("#title-edit"),
+        $titleNoButton=$("#title-no-btn"),
+        $titleEditForm=$("#title-edit-form"),
+        $sortable=$(".sortable");
+
+    //handler for the edit title button being clicked
+    $showTitleEditFormButton.click(function () {
+        $titleEditDone.hide();
+        $titleEditForm.show();
+
+    });
+
+    //handler to hide the title edit form
+    $titleNoButton.click(function(){
+        $titleEditForm.hide();
+        $titleEditDone.show();
+    });
+
+    $sortable.sortable({
         placeholder: "ui-state-highlight"
     });
-    $( "#sortable" ).disableSelection();
+    $sortable.disableSelection();
 });
