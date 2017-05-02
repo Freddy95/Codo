91d4b40e7073f4cfe1546293536d4b4273d56310
Henry <henry.chin@stonybrook.edu>
2017-05-02 17:28:06 -0400

    Added stuff for saving edited lesson

--- src/main/webapp/public/js/edit-lesson.js
+++ src/main/webapp/public/js/edit-lesson.js
@@ -2,36 +2,37 @@
  * Created by Matt on 4/27/17.
  * Javascript for the edit lesson page
  */
-$(function(){
-    //get the elements we need to build the session object
-    var $publicRadio = $("#publicradio") ,
-        $descriptionText=$("#description-text"),
-        $submiteDescButton=$("#submit-description-button"),
-        $titleText=$("#title-edit-form"),
-        $titleSubmitButton=$("#title-submit-btn"),
-        $taskList=$("#tasks-list"),
-        $titleEditDone=$("#title-done"),
-        $titleEditButton=$("#title-button"),
-        $showTitleEditFormButton=$("#title-edit"),
-        $titleNoButton=$("#title-no-btn"),
-        $titleEditForm=$("#title-edit-form"),
-        $sortable=$(".sortable");
 
-    //handler for the edit title button being clicked
-    $showTitleEditFormButton.click(function () {
-        $titleEditDone.hide();
-        $titleEditForm.show();
-
-    });
-
-    //handler to hide the title edit form
-    $titleNoButton.click(function(){
-        $titleEditForm.hide();
-        $titleEditDone.show();
-    });
-
-    $sortable.sortable({
-        placeholder: "ui-state-highlight"
-    });
-    $sortable.disableSelection();
+$(document).ready(function() {
+  init();
 });
+
+var tasksToDelete=[];
+
+// Convenience methood to get value from a placeholder.
+
+function init() {
+    $('.sortable').sortable({
+        placeholder: "ui-state-highlight"
+    }).disableSelection();
+};
+
+// Save data.
+function save() {
+    var data = {};
+
+    // Delete every task first.
+    for(taskToDeleteInc = 0; taskToDeleteInc < tasksToDelete.length; taskToDeleteInc++) {
+        //TODO: Send delete request for each task.
+        console.log(tasksToDelete[taskToDeleteInc])
+    }
+
+    // Grab detail fields.
+    data.title = $('#title').val();
+    data.description = $('#description').val();
+    data.shared = $('#shared').is(":checked");
+
+    // Grab current order of tasks.
+
+    console.log(data);
+}
