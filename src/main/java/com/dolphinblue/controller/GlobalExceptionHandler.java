a88f0a51cf4717e5dd46f3375e02942c6cf5a989
Matt Del Signore <mattdelsig@me.com>
2017-05-03 22:24:34 -0400

    added 404 page

---
+++ src/main/java/com/dolphinblue/controller/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.dolphinblue.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Created by Matt on 5/3/17.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * Handle a not found exception and return a 404
+     * @return the 404 template
+     */
+    @ExceptionHandler(value = NullPointerException.class)
+    public String fourohfour(){
+        return "404";
+    }
+}
