fc91fe938fb6957227815812f60a5a8cf2254a7f
Festevez <f.estevez95@gmail.com>
2017-05-17 09:08:01 -0400

    remove @cache

--- src/main/java/com/dolphinblue/models/Lesson.java
+++ src/main/java/com/dolphinblue/models/Lesson.java
@@ -1,6 +1,7 @@
 package com.dolphinblue.models;
 
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
